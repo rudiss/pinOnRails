4b91c9c8a4ff4f5a2699d0a3b36e5d7454d531ae
rudis <rudislpp@gmail.com>
2016-05-22 17:47:26 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACESS_KEY_ID'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
